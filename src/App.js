582c1d62e3575d152a8d5eb4504cdd5372a98559
HaseebullahMiakhil <tw773029@gmail.com>
2024-03-03 22:03:07 -0800

    first changes

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ExpenseItem from './components/ExpenseItem';
+
+
+function App() {
+  return (
+    <div>
+    <h1>my first website</h1>
+      <p>this is also avaliable</p>
+      <ExpenseItem></ExpenseItem>
+    </div>
+     
+  );
+}
+
+export default App;
+ 
